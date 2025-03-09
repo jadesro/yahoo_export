dd8733e1398de362a5e6c44ed948d343f2e3de4d
Jacques <jacques@desroches.ca>
2025-03-09 12:41:21 -0400

    Now using loadenv

--- src/yahoo_export/main.py
+++ src/yahoo_export/main.py
@@ -9,6 +9,8 @@
 
 
 import pandas as pd
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 # TODO: This is a TODO
@@ -35,8 +37,11 @@ def export_as_csv(df: pd.DataFrame, filename: str) -> None:
 
 def yahoo_export():
     print("Export finance DB to yahoo via csv")
-    url = "mysql+pymysql://jacques:1Kermit1@192.168.1.20/finance?charset=utf8mb4"
-    tickers = read_from_db(url, "Export from local DB")
+    load_dotenv()
+    #url = "mysql+pymysql://jacques:1Kermit1@192.168.1.20/finance?charset=utf8mb4"
+    db_url = os.getenv("DATABASE_URL")
+    print(f"DB URL is: {db_url}")
+    tickers = read_from_db(db_url, "Export from local DB")
     export_as_csv(tickers, "yahoo.csv")
     print(f"Done. File yahoo.csv created.")
 
